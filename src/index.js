3aae6a6e0ac62b16620bcc6dc06df75df529622c
elsehow <yes@cosmopol.is>
2016-04-02 13:30:34 -0700

    minimal working specs
    initial
    
    added travis file
    
    minimal working spec
    
    removed manager db
    
    starting readme and some docs
    
    full specs and tests
    
    watch will now watch all js files
    
    fixed empty module
    
    fixed up docs
    
    using tape as a dev dependency now.
    
    now, npm test calls tape/*.js (this allows us to run multiple files)
    
    moved to runwatch dep
    
    updated runwatch dep
    
    turned file glob into string for runwatch
    
    fixed dumb quotes error
    
    Added a details.md section to my empty readme
    
    Added boxels-to-dna-order
    Includes README
    
    details
    
    reverted
    
    cleaned
    
    updated readme
    
    ..
    
    initial
    
    readme
    
    cleaned
    
    updated package name
    
    removed music dir

---
+++ src/index.js
@@ -0,0 +1,84 @@
+var mm = require('musicmetadata');
+let last = arr => arr[arr.length-1]
+var join = require('path').join
+var readdir = require('fs').readdir
+var read = require('fs').createReadStream
+var kefir = require('kefir')
+let truthy = x => !!x
+var write = require('fs').writeFile
+
+function mdataS (hash, path) {
+  return kefir.fromNodeCallback(cb => {
+    return mm(read(path), (err, res) => {
+      if (err || !res.title) return cb()
+      res.ipfsHash = hash
+      return cb(null,res)
+    })
+  })
+}
+
+function mdataFromS (ipfsData) {
+  return mdataS(ipfsData.hash, ipfsData.path)
+}
+
+function albumMetadata (songsMetadata) {
+  let exemplar = songsMetadata[0]
+  return {
+    tracks: songsMetadata,
+    album: exemplar.album,
+    artist: exemplar.artist,
+    year: exemplar.year,
+  }
+}
+
+// stream of `ipfs add` results
+function addToIpfsS (ipfs, path) {
+  return kefir.fromNodeCallback(cb => {
+    ipfs.util.addFromFs(path, cb)
+  }).map(r => r[0])
+}
+
+function writeS (path, jsobj) {
+  return kefir.fromNodeCallback(cb => {
+    return write(path,
+                 JSON.stringify(jsobj),
+                 cb)
+  })
+}
+
+// stream of `ipfs add` results
+function addDirToIpfsS (ipfs, dir) {
+  return kefir.fromNodeCallback(cb => {
+    ipfs.util.addFromFs(dir, {
+      recursive: true,
+    }, cb)
+  })
+}
+
+function addAll (ipfs, dir, cb) {
+  return readdir(dir, (err, files) => {
+    let paths = files.map(fn => join(dir, fn))
+    let ipfsDataS = kefir.combine(
+      paths.map(path => addToIpfsS(ipfs,path))
+    )
+    let tracksJsonPath = join(dir, 'tracks.json')
+
+    let allTracksMetadataS = ipfsUploadedFiles =>
+        kefir.combine(
+          ipfsUploadedFiles
+            .map(mdataFromS))
+
+    ipfsDataS
+      .flatMap(allTracksMetadataS)
+      .map(lst => lst.filter(truthy))
+      .flatMap(lst => writeS(tracksJsonPath, lst))
+      .flatMap(_ => addDirToIpfsS(ipfs, dir).map(last).map(x => x.hash))
+      .onValue(x => cb(null, x))
+
+    ipfsDataS
+      .onError(cb)
+
+  })
+}
+
+module.exports = addAll
---
+++ test/index.js
@@ -0,0 +1,11 @@
+var join = require('path').join
+var exampleFolder = 'example-music-dir'
+var dir = join(__dirname,exampleFolder)
+var ipfsAPI = require('ipfs-api')
+// connect to ipfs daemon API server
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'}) 
+var metadata = require('..')
+metadata(ipfs, dir, (err, res) => {
+  if (err) throw err
+  else console.log(res)
+})
